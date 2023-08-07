9239e468e698fd528fd044513e756bbdef8e43ad
Jabed Hasan <60073739+JabedHossainJabuu@users.noreply.github.com>
2023-08-08 01:47:53 +0600

    registration, login done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,16 +4,16 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
+  
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Fonts -->
+
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
-    <!-- Scripts -->
+   
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
